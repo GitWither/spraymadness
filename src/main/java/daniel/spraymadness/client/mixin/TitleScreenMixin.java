991b8b7d6e47ca5a684fd764ed624fa631b668ab
GitWither <wither.tweets@gmail.com>
2022-04-03 16:27:34 +0200

    Adjusted spray can texture

--- src/main/java/daniel/spraymadness/client/mixin/TitleScreenMixin.java
+++ src/main/java/daniel/spraymadness/client/mixin/TitleScreenMixin.java
@@ -12,13 +12,16 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
+    @Unique
     private static final TranslatableText TOOLTIP = new TranslatableText("gui.spray_madness.spray_gallery.title");
+    @Unique
     private static final Identifier SPRAY_CAN = new Identifier(SprayMadness.MOD_ID, "textures/gui/prototypes/spray_can_vertical.png");
 
     protected TitleScreenMixin(Text title) {
@@ -27,7 +30,7 @@ public abstract class TitleScreenMixin extends Screen {
 
     @Inject(method = "init", at = @At("HEAD"))
     public void addSprayGalleryButton(CallbackInfo cb) {
-        this.addDrawableChild(new TexturedButtonWidget(this.width / 2 + 150, this.height / 4 + 132, 20, 20, 0, 0, 20, SPRAY_CAN, 16, 16, (button) -> {
+        this.addDrawableChild(new TexturedButtonWidget(this.width / 2 + 150, this.height / 4 + 132, 20, 20, 0, 0, 20, SPRAY_CAN, 32, 64, (button) -> {
             this.client.setScreen(new SprayGalleryScreen());
         }, (button, matrices, mouseX, mouseY) -> {
             TitleScreenMixin.super.renderTooltip(matrices, TOOLTIP, mouseX, mouseY);
